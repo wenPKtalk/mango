347301df4060bc71ab3fab9a763f5ea00459c03b
wenpengkun <wenpengkun@funstory.ai>
2021-01-07 15:48:46 +0800

    Add docker compose file to add mysql

--- guifei-mango/src/main/java/com/free/guifei/controller/GuifeiMangoApplication.java
+++ guifei-mango/src/main/java/com/free/guifei/controller/GuifeiMangoApplication.java
@@ -1,4 +1,4 @@
-package com.free.guifei;
+package com.free.guifei.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
