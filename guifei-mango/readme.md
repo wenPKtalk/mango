3f5dd855cf72f40143e34827c82259a33203e1c8
wenpengkun <wenpengkun@funstory.ai>
2021-01-07 14:06:28 +0800

    Add help file

---
+++ guifei-mango/readme.md
@@ -0,0 +1,12 @@
+# Read Me First
+spring boot项目。启动前请执行
+mac/linux
+```
+./gradlew idea
+```
+windows
+```
+gradlew.bat idea
+```
+生成idea.ipr文件后双击打开即可。
+![图](idea.jpg)
--- guifei-mango/src/main/java/com/free/guifei/GuifeiMangoApplication.java
+++ guifei-mango/src/main/java/com/free/guifei/GuifeiMangoApplication.java
@@ -2,7 +2,6 @@ package com.free.guifei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication(scanBasePackages = "com.free.guifei")
 public class GuifeiMangoApplication {
